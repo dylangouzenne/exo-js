405b6a2f5f20eb8838dfd411a4a29bedb116910c
dylangouzenne <dylan.gouzenne@gmail.com>
2018-06-11 16:40:02 +0200

    commit exo4

---
+++ exo4.js
@@ -0,0 +1,9 @@
+function test1() 
+{ 
+var nom = document.getElementById("nom").value; 
+var prenom = document.getElementById("prenom").value;
+var ville = document.getElementById("ville").value;
+
+alert("Nom : "+ nom +"\nPrénom : "+ prenom +"\nVille : "+ ville );
+} 
+
