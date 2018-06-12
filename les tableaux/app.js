98608c6cd7375b409741320fc8cac15cbe1e0dc1
dylangouzenne <dylan.gouzenne@gmail.com>
2018-06-12 15:28:23 +0200

    les tableaux exo6

--- les tableaux/app.js
+++ les tableaux/app.js
@@ -1,6 +1,4 @@
 var calendrier = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "décembre"];
 
 
-calendrier.splice(7, 7, 'août');
-
-document.write (calendrier[7]);
+document.write(calendrier);
