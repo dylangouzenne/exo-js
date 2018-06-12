1445b4bd2a0a4b9057bcca9bf067b2fa5d239dcb
dylangouzenne <dylan.gouzenne@gmail.com>
2018-06-12 12:16:04 +0200

    les tableaux exo3

--- les tableaux/app.js
+++ les tableaux/app.js
@@ -1,3 +1,3 @@
 var calendrier = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "décembre"];
 
-document.write (calendrier[2]);
+document.write (calendrier[5]);
