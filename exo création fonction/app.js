de8e25346dc93337d33882bb51d2bdd59b4cc825
dylangouzenne <dylan.gouzenne@gmail.com>
2018-06-12 12:04:52 +0200

    exo création fonction

---
+++ exo création fonction/app.js
@@ -0,0 +1,5 @@
+var pointure = prompt("pointure ");
+var naissance = prompt("naissance ?");
+var result = calculer(pointure, naissance);
+
+alert("mon résultat est : "+resultat+" !")
---
+++ exo création fonction/fonction.js
@@ -0,0 +1,5 @@
+function calculer(p, n)
+{
+	var result = (parsint(p)*2+5)*50- parsint(n)+1766;
+	return result;
+}
